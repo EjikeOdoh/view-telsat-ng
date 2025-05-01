4bdc1a75605e973fd9594ee65552a190a3ea7fc7
EjikeOdoh <ejike.developer@gmail.com>
2025-05-01 10:18:30 +0100

    Add whatsapp links

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuLinks = document.querySelectorAll('.mobile-menu a')
+const menuBtnText = document.querySelector('.menu-btn span')
+const productsContainer = document.querySelector('.more-products')
+
+const productsBtn = document.querySelector('.products > button')
+
+function toggleMenu(e) {
+    e.stopPropagation();
+    const isHidden = mobileMenu.classList.contains('hidden')
+    if (isHidden) {
+        mobileMenu.classList.remove('hidden')
+        menuBtnText.textContent = 'close'
+
+    } else {
+        mobileMenu.classList.add('hidden')
+        menuBtnText.textContent = 'menu'
+    }
+}
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        mobileMenu.classList.add('hidden')
+    })
+})
+
+window.addEventListener('click', (e)=>{
+    if (!e.target.matches('div.mobile-menu, header')) {
+        mobileMenu.classList.add('hidden')
+        menuBtnText.textContent = 'menu'
+    }
+})
+
+function toggleProducts() {
+    const isHidden = productsContainer.classList.contains('hide-products')
+    if (isHidden) {
+        productsContainer.classList.remove('hide-products')
+        productsBtn.textContent =  `Show less`
+    } else {
+        productsContainer.classList.add('hide-products')
+        productsBtn.textContent = `Show more`
+    }
+}
